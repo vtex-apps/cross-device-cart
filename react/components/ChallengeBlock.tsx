a3991c066960aab9c1e0acb54909456d27d8b841
Santiago Sanz <santiago.sanz@vtex.com.br>
2022-01-19 00:36:16 -0300

    fix: challenge block UI

--- react/components/ChallengeBlock.tsx
+++ react/components/ChallengeBlock.tsx
@@ -32,13 +32,12 @@ const ChallengeBlock: FC<Props> = ({
     const classes =
       type === 'floatingBar'
         ? 'shadow-2 pa5 fixed bottom-0 z-999 left-0'
-        : `
-  } flex items-center justify-center pa4 tc ${
-    device === 'phone' ? 'flex-column' : ''
-  }`
+        : `pa4 tc ${device === 'phone' ? 'flex-column' : ''}`
 
     return (
-      <div className={`${classes} w-100 bg-base`}>
+      <div
+        className={`${classes} w-100 bg-base flex items-center justify-center`}
+      >
         <span className={`t-small ${device === 'phone' ? 'pb3' : ''}`}>
           It seems you left some item/s in another device. Do you want to
           recover them?
@@ -93,37 +92,26 @@ const ChallengeBlock: FC<Props> = ({
         onClose={() => {
           handleModal(false)
         }}
+        bottomBar={
+          <div className="nowrap">
+            <Button
+              size="small"
+              variation="secondary"
+              onClick={() => {
+                handleAccept(toastHandler)
+              }}
+              isLoading={mutationLoading}
+            >
+              Do it
+            </Button>
+          </div>
+        }
       >
-        <div className="dark-gray">
-          <span className="t-small">
+        <div className="dark-gray pv7">
+          <span>
             It seems you left some item/s in another device. Do you want to
             recover them?
           </span>
-          <div className="flex">
-            <span className="mh4">
-              <Button
-                size="small"
-                variation="secondary"
-                onClick={() => {
-                  handleAccept(toastHandler)
-                }}
-                isLoading={mutationLoading}
-              >
-                Do it
-              </Button>
-            </span>
-            <span>
-              <ButtonWithIcon
-                size="small"
-                icon={close}
-                variation="tertiary"
-                onClick={() => {
-                  handleDecline()
-                }}
-                isLoading={mutationLoading}
-              />
-            </span>
-          </div>
         </div>
       </Modal>
     )
