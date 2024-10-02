8c87aa17446773d22052c10ac2f2d33d9e5f0196
Alex Efremov <efremov.sakha@gmail.com>
2024-10-02 17:37:47 +0700

    Update the strategy logic in the replaceCart method

--- node/resolvers/replaceCart.ts
+++ node/resolvers/replaceCart.ts
@@ -21,24 +21,33 @@ export const replaceCart = async (
 
     response.set(
       'set-cookie',
-      `checkout.vtex.com=__ofid=${savedCart}; Max-Age=15552000; Domain=${host}; path=/; secure; samesite=lax; httponly`
+      `checkout.vtex.com=__ofid=${currentCart}; Max-Age=15552000; Domain=${host}; path=/; secure; samesite=lax; httponly`
     )
 
-    const orderForm = await checkoutIO.getOrderForm(savedCart)
+    const savedItems = await checkoutIO.getItems(savedCart)
 
-    if (strategy !== 'REPLACE') {
+    if (strategy === 'REPLACE') {
+      if (!savedItems.length) {
+        return await checkoutIO.getOrderForm(currentCart)
+      }
+
+      await requestHub.clearCart(currentCart)
+
+      return await checkoutIO.addToCart(currentCart, savedItems)
+    } else {
       /**
        * Add to cart has a specific graphql INPUT type.
        * These calls ensure handling correct types from start to finish.
        */
-      const savedItems = await checkoutIO.getItems(savedCart)
       const currentItems = await checkoutIO.getItems(currentCart)
 
       const tally = strategy === 'COMBINE'
 
       const items = mergeItems(currentItems, savedItems, tally)
 
-      if (!items.length) return orderForm
+      if (!items.length) {
+        return await checkoutIO.getOrderForm(currentCart)
+      }
 
       await requestHub.clearCart(currentCart)
 
@@ -47,12 +56,9 @@ export const replaceCart = async (
         element.index = index
       })
 
-      const newOrderForm = await checkoutIO.addToCart(currentCart, items)
-
-      return newOrderForm
+      return await checkoutIO.addToCart(currentCart, items)
     }
 
-    return orderForm
 
   } else {
     return null
